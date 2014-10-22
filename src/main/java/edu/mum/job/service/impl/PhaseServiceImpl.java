8c79e1e9f7ef6617fbf44c96d92870a151ad3d6b
adiyafre <adiyafre@gmail.com>
2014-10-21 22:10:51 -0500

    Phase model added
    please use phase.sql for db schema, and use getPhase(phase_id) method
    for retrieve phase model. and use getphase_type() method for retrieve
    phase type

---
+++ src/main/java/edu/mum/job/service/impl/PhaseServiceImpl.java
@@ -0,0 +1,26 @@
+package edu.mum.job.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.mum.job.domain.Event;
+import edu.mum.job.domain.Phase;
+import edu.mum.job.repository.EventRepository;
+import edu.mum.job.repository.PhaseRepository;
+import edu.mum.job.service.PhaseService;
+
+@Service
+@Transactional
+public class PhaseServiceImpl implements PhaseService {
+	
+	@Autowired
+	PhaseRepository phaseRepository;
+		
+	@Override
+	public Phase getPhase(int phaseID) {
+		return phaseRepository.getPhase(phaseID);
+	}
+	
+	
+}
